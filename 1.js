ca44bbfb61d6976298587fba8b986c0c0fc8546b
johnmogi <anguru@gmail.com>
2021-07-08 19:53:59 +0300

    added

---
+++ 1.js
@@ -0,0 +1,8 @@
+git config user.name
+
+https://docs.github.com/en/get-started/getting-started-with-git/setting-your-username-in-git
+
+git config user.name "Mona Lisa"
+
+git config user.email "shaked@gmail.com"
+
